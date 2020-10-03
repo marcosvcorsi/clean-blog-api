727e54c3de412a34912c890bb3366cc09c55a825
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-02 23:19:10 -0300

    test: ensure UsersRepository insert a new user

---
+++ src/infra/database/typeorm/repositories/UsersRepository.spec.ts
@@ -0,0 +1,36 @@
+import { mockCreateUserParams } from '@/domain/test';
+import { Connection } from 'typeorm';
+import createConnection from '../index';
+import { UsersRepository } from './UsersRepository';
+
+let connection: Connection;
+
+const makeSut = () => {
+  return new UsersRepository();
+};
+
+describe('UsersRepository Test', () => {
+  beforeAll(async () => {
+    connection = await createConnection('test');
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM users');
+  });
+
+  afterAll(async () => {
+    await connection.query('DELETE FROM users');
+
+    await connection.close();
+  });
+
+  it('should be able to insert a new user', async () => {
+    const sut = makeSut();
+
+    const user = await sut.create(mockCreateUserParams());
+
+    expect(user).toBeTruthy();
+    expect(user).toHaveProperty('id');
+    expect(user.id).toBeTruthy();
+  });
+});
