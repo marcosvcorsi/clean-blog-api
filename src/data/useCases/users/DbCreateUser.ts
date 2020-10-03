094597e645efc7f9a96fe8d3062e43ba01ba5339
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-02 21:29:57 -0300

    feat: ensure DbCreateUser returns user on success

--- src/data/useCases/users/DbCreateUser.ts
+++ src/data/useCases/users/DbCreateUser.ts
@@ -6,8 +6,8 @@ export class DbCreateUser implements CreateUser {
   constructor(private readonly createUserRepository: CreateUserRepository) {}
 
   async create(data: CreateUserParams): Promise<UserModel> {
-    await this.createUserRepository.create(data);
+    const user = await this.createUserRepository.create(data);
 
-    return {} as UserModel;
+    return user;
   }
 }
