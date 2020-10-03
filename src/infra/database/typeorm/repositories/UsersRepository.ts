f482f7bc0c5c3f836d43ae51d47955b781118bc9
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-02 23:18:56 -0300

    feat: ensure UsersRepository insert a new user

---
+++ src/infra/database/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,20 @@
+import { CreateUserRepository } from '@/data/protocols/users/CreateUserRepository';
+import { CreateUserParams } from '@/domain/useCases/CreateUser';
+import { getRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
+
+export class UsersRepository implements CreateUserRepository {
+  private readonly usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getRepository(User);
+  }
+
+  async create(data: CreateUserParams): Promise<User> {
+    const user = this.usersRepository.create(data);
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
