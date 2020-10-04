9a3f0d886a992cacface1d767061923beb9e2a59
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-04 20:59:01 -0300

    feat: add bcrypt adapter to makeDbCreateUser

--- src/main/app/factories/useCases/users/dbCreateUser.ts
+++ src/main/app/factories/useCases/users/dbCreateUser.ts
@@ -1,8 +1,12 @@
 import { DbCreateUser } from '@/data/useCases/users/DbCreateUser';
+import { BcryptAdapter } from '@/infra/crypto/bcryptAdapter/BcryptAdapter';
 import { UsersRepository } from '@/infra/database/typeorm/repositories/UsersRepository';
 
 export const makeDbCreateUser = () => {
+  const salt = 12;
+
+  const bcryptAdapter = new BcryptAdapter(salt);
   const usersRepository = new UsersRepository();
 
-  return new DbCreateUser(usersRepository);
+  return new DbCreateUser(bcryptAdapter, usersRepository);
 };
