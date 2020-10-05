a31c9618aec40842316dbb7c317d03e1b489264d
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-04 22:46:25 -0300

    feat: ensure DbCreateUser calls Mail

---
+++ src/infra/mail/NodemailerAdapter.ts
@@ -0,0 +1,38 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import { Mail, MailParams } from '@/data/protocols/mail/Mail';
+
+export class NodemailerAdapter implements Mail {
+  private client: Transporter;
+
+  async getTransporter() {
+    if (!this.client) {
+      const account = await nodemailer.createTestAccount();
+
+      this.client = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: account.user, // generated ethereal user
+          pass: account.pass, // generated ethereal password
+        },
+      });
+    }
+
+    return this.client;
+  }
+
+  async sendMail({ to, subject, html }: MailParams): Promise<void> {
+    const transporter = await this.getTransporter();
+
+    return transporter.sendMail({
+      from: {
+        name: 'Clean Blog',
+        address: 'cleanblogapi@test.com',
+      },
+      to,
+      subject,
+      html,
+    });
+  }
+}
