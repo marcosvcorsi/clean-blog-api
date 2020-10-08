3d3160955bd35b4c1abc33ff7cd91f8ed37383bf
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-07 21:49:46 -0300

    feat: ensure LoginController return serverError Authentication throws

--- src/presentation/controllers/users/login/LoginController.ts
+++ src/presentation/controllers/users/login/LoginController.ts
@@ -1,4 +1,5 @@
 import { IAuthentication } from '@/domain/useCases/IAuthentication';
+import { serverError } from '@/presentation/helpers/http';
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/Http';
 import { IController } from '@/presentation/protocols/IController';
 
@@ -6,10 +7,14 @@ export class LoginController implements IController {
   constructor(private readonly authentication: IAuthentication) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body;
+    try {
+      const { email, password } = httpRequest.body;
 
-    await this.authentication.auth({ email, password });
+      await this.authentication.auth({ email, password });
 
-    return {} as HttpResponse;
+      return {} as HttpResponse;
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
