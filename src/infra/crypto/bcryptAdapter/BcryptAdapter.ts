5cc9e348adf5b511621f9d7153d077c8f963b2b3
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-07 22:24:36 -0300

    feat: ensure BcryptAdapter compare calls bcrypt compare

--- src/infra/crypto/bcryptAdapter/BcryptAdapter.ts
+++ src/infra/crypto/bcryptAdapter/BcryptAdapter.ts
@@ -1,12 +1,19 @@
 import bcrypt from 'bcrypt';
 import { IHasher } from '@/data/protocols/crypto/IHasher';
+import { IHashComparer } from '@/data/protocols/crypto/IHashComparer';
 
-export class BcryptAdapter implements IHasher {
+export class BcryptAdapter implements IHasher, IHashComparer {
   constructor(private readonly salt: number) {}
 
   async generate(value: string): Promise<string> {
     const hashedValue = await bcrypt.hash(value, this.salt);
 
     return hashedValue;
+  }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    const match = await bcrypt.compare(value, hash);
+
+    return match;
   }
 }
