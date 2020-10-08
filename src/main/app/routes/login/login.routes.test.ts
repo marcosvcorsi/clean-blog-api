04615183139b7ca743d0a082452e5c055dc88663
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-07 22:42:00 -0300

    test: ensure login route authenticate with valid credentials

--- src/main/app/routes/login/login.routes.test.ts
+++ src/main/app/routes/login/login.routes.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest';
-import { Connection } from 'typeorm';
+import { hash } from 'bcrypt';
+import { Connection, getRepository } from 'typeorm';
+import { User } from '@/infra/database/typeorm/entities/User';
 import createConnection from '@/infra/database/typeorm/connection';
 import app from '../../index';
 
@@ -27,5 +29,32 @@ describe('Login Route Test', () => {
     });
 
     expect(response.status).toBe(401);
+  });
+
+  it('should be able to authenticate with valid credentials', async () => {
+    const email = 'anymail@mail.com';
+    const password = 'anypassword';
+    const salt = 12;
+
+    const hashedPassword = await hash(password, salt);
+
+    const userRepository = getRepository(User);
+
+    const fakeUser = userRepository.create({
+      name: 'anyname',
+      email,
+      password: hashedPassword,
+    });
+
+    await userRepository.save(fakeUser);
+
+    const response = await request(app).post('/login').send({
+      email,
+      password: 'anypassword',
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body.email).toBe(email);
+    expect(response.body).toHaveProperty('token');
   });
 });
