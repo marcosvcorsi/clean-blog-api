4bb704db1270a9d092c4bb13317cbcaa6f21745f
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-09 22:42:52 -0300

    fix: remove unecessary config

--- src/main/config/mail.ts
+++ src/main/config/mail.ts
@@ -1,7 +1,7 @@
 export default {
   host: String(process.env.MAIL_HOST),
   port: Number(process.env.MAIL_PORT),
-  secure: Boolean(process.env.MAIL_SECURE || false),
+  secure: Boolean(process.env.MAIL_SECURE),
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
