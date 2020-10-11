975cec1f232556648cc57859d3f114affa50e392
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-10 23:06:49 -0300

    refactor: change to user option chaining

--- src/data/useCases/users/crypto/DecryptUserIdByToken.ts
+++ src/data/useCases/users/crypto/DecryptUserIdByToken.ts
@@ -7,12 +7,6 @@ export class DecryptUserIdByToken implements ILoadUserIdByToken {
   async loadUserId(token: string): Promise<number | null> {
     const payload = await this.decrypter.decrypt(token);
 
-    if (payload) {
-      const { userId } = payload;
-
-      return userId;
-    }
-
-    return null;
+    return payload?.userId;
   }
 }
