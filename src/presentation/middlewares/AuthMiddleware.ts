739d2b5f5393f26c13c1a5fb343427b03a5b5808
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-11 00:12:52 -0300

    feat: ensure AuthMiddleware return serverError

--- src/presentation/middlewares/AuthMiddleware.ts
+++ src/presentation/middlewares/AuthMiddleware.ts
@@ -1,6 +1,6 @@
 import { ILoadUserIdByToken } from '@/domain/useCases/users/ILoadUserIdByToken';
 import { AccessDeniedError } from '../errors/AccessDeniedError';
-import { forbidden, ok } from '../helpers/http';
+import { forbidden, ok, serverError } from '../helpers/http';
 import { HttpRequest, HttpResponse } from '../protocols/Http';
 import { IMiddleware } from '../protocols/IMiddleware';
 
@@ -8,16 +8,20 @@ export class AuthMiddleware implements IMiddleware {
   constructor(private readonly loadUserIdByToken: ILoadUserIdByToken) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.authorization;
+    try {
+      const accessToken = httpRequest.headers?.authorization;
 
-    if (!accessToken) {
-      return forbidden(new AccessDeniedError());
+      if (!accessToken) {
+        return forbidden(new AccessDeniedError());
+      }
+
+      const [, token] = accessToken.split(' ');
+
+      await this.loadUserIdByToken.loadUserId(token);
+
+      return ok({ userId: 1 });
+    } catch (error) {
+      return serverError(error);
     }
-
-    const [, token] = accessToken.split(' ');
-
-    await this.loadUserIdByToken.loadUserId(token);
-
-    return ok({ userId: 1 });
   }
 }
