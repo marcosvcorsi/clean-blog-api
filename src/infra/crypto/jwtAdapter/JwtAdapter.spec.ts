25426918542514bce96ddb4375be7bfff18ab67b
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-10 23:10:52 -0300

    refactor: move Jwt sign test to a describe

--- src/infra/crypto/jwtAdapter/JwtAdapter.spec.ts
+++ src/infra/crypto/jwtAdapter/JwtAdapter.spec.ts
@@ -12,31 +12,33 @@ const makeSut = () => {
 };
 
 describe('JwtAdapter Test', () => {
-  it('should call jwt sign with correct value', async () => {
-    const sut = makeSut();
+  describe('sign()', () => {
+    it('should call jwt sign with correct value', async () => {
+      const sut = makeSut();
 
-    const jwtSpy = jest.spyOn(jwt, 'sign');
+      const jwtSpy = jest.spyOn(jwt, 'sign');
 
-    await sut.encrypt(1);
+      await sut.encrypt(1);
 
-    expect(jwtSpy).toHaveBeenCalledWith({ id: 1 }, 'secret');
-  });
+      expect(jwtSpy).toHaveBeenCalledWith({ id: 1 }, 'secret');
+    });
 
-  it('should throw if jwj sign throws', async () => {
-    const sut = makeSut();
+    it('should throw if jwt sign throws', async () => {
+      const sut = makeSut();
 
-    jest
-      .spyOn(jwt, 'sign')
-      .mockImplementationOnce(() => Promise.reject(new Error()));
+      jest
+        .spyOn(jwt, 'sign')
+        .mockImplementationOnce(() => Promise.reject(new Error()));
 
-    await expect(sut.encrypt(1)).rejects.toThrow();
-  });
+      await expect(sut.encrypt(1)).rejects.toThrow();
+    });
 
-  it('should return a token on success', async () => {
-    const sut = makeSut();
+    it('should return a token on success', async () => {
+      const sut = makeSut();
 
-    const response = await sut.encrypt(1);
+      const response = await sut.encrypt(1);
 
-    expect(response).toBe('anytoken');
+      expect(response).toBe('anytoken');
+    });
   });
 });
