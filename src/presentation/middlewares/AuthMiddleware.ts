c65e7384107d1c191c2a619aeb74420d0ef85649
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-11 00:09:01 -0300

    feat: ensure AuthMiddleware call LoadUserIdByToken

--- src/presentation/middlewares/AuthMiddleware.ts
+++ src/presentation/middlewares/AuthMiddleware.ts
@@ -1,15 +1,22 @@
+import { ILoadUserIdByToken } from '@/domain/useCases/users/ILoadUserIdByToken';
 import { AccessDeniedError } from '../errors/AccessDeniedError';
 import { forbidden, ok } from '../helpers/http';
 import { HttpRequest, HttpResponse } from '../protocols/Http';
 import { IMiddleware } from '../protocols/IMiddleware';
 
 export class AuthMiddleware implements IMiddleware {
+  constructor(private readonly loadUserIdByToken: ILoadUserIdByToken) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.authorization;
 
     if (!accessToken) {
       return forbidden(new AccessDeniedError());
     }
+
+    const [, token] = accessToken.split(' ');
+
+    await this.loadUserIdByToken.loadUserId(token);
 
     return ok({ userId: 1 });
   }
