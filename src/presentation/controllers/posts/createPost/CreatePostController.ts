92d5b45aebc1fc08621c3a06b86e2ae5d1dbd0eb
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-11 22:26:21 -0300

    feat: ensure CreatePostController returns created with post

--- src/presentation/controllers/posts/createPost/CreatePostController.ts
+++ src/presentation/controllers/posts/createPost/CreatePostController.ts
@@ -1,5 +1,5 @@
 import { ICreatePost } from '@/domain/useCases/posts/ICreatePost';
-import { serverError } from '@/presentation/helpers/http';
+import { created, serverError } from '@/presentation/helpers/http';
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/Http';
 import { IController } from '@/presentation/protocols/IController';
 
@@ -11,14 +11,14 @@ export class CreatePostController implements IController {
       const { userId } = httpRequest;
       const { title, content } = httpRequest.body;
 
-      await this.createPost.create({
+      const post = await this.createPost.create({
         userId: Number(userId),
         title,
         content,
         date: new Date(),
       });
 
-      return {} as HttpResponse;
+      return created(post);
     } catch (error) {
       return serverError(error);
     }
