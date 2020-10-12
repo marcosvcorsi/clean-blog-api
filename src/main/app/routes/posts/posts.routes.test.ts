f13ac81ec73468eee6fe8e99848ab8d0d38d8db0
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-11 21:44:58 -0300

    test: ensure posts routes return forbidden without authorization

---
+++ src/main/app/routes/posts/posts.routes.test.ts
@@ -0,0 +1,33 @@
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import createConnection from '@/infra/database/typeorm/connection';
+import app from '../../index';
+
+let connection: Connection;
+
+describe('Posts Routes Test', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM posts');
+    await connection.query('DELETE FROM users');
+  });
+
+  afterAll(async () => {
+    await connection.query('DELETE FROM posts');
+    await connection.query('DELETE FROM users');
+
+    await connection.close();
+  });
+
+  it('should return forbidden on POST /posts without Authorization', async () => {
+    const response = await request(app).post('/posts').send({
+      title: 'anytitle',
+      content: 'anycontent',
+    });
+
+    expect(response.status).toBe(403);
+  });
+});
