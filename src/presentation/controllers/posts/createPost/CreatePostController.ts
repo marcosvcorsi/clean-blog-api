d95bf665af298c59e8d5988b887c84918137d186
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-11 22:21:08 -0300

    feat: ensure CreatePostController return serverError if CreatePost throws

--- src/presentation/controllers/posts/createPost/CreatePostController.ts
+++ src/presentation/controllers/posts/createPost/CreatePostController.ts
@@ -1,4 +1,5 @@
 import { ICreatePost } from '@/domain/useCases/posts/ICreatePost';
+import { serverError } from '@/presentation/helpers/http';
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/Http';
 import { IController } from '@/presentation/protocols/IController';
 
@@ -6,16 +7,20 @@ export class CreatePostController implements IController {
   constructor(private readonly createPost: ICreatePost) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { userId } = httpRequest;
-    const { title, content } = httpRequest.body;
+    try {
+      const { userId } = httpRequest;
+      const { title, content } = httpRequest.body;
 
-    await this.createPost.create({
-      userId: Number(userId),
-      title,
-      content,
-      date: new Date(),
-    });
+      await this.createPost.create({
+        userId: Number(userId),
+        title,
+        content,
+        date: new Date(),
+      });
 
-    return {} as HttpResponse;
+      return {} as HttpResponse;
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
