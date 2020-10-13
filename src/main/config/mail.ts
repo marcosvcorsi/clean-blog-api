c7df5e7b4310caf008acaed017d68f592bea96e4
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-12 22:13:41 -0300

    fix: nodemailer send fake email

--- src/main/config/mail.ts
+++ src/main/config/mail.ts
@@ -1,7 +1,6 @@
 export default {
   host: String(process.env.MAIL_HOST),
   port: Number(process.env.MAIL_PORT),
-  secure: Boolean(process.env.MAIL_SECURE),
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
