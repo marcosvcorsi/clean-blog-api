6e5340469b1bef236d4e5e497a81cea3c7e951f5
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-14 22:34:15 -0300

    feat: ensure RedisAdapter load calls client get

--- src/infra/cache/redisAdapter/RedisAdapter.ts
+++ src/infra/cache/redisAdapter/RedisAdapter.ts
@@ -1,8 +1,9 @@
 import { Redis as RedisClient } from 'ioredis';
 import { IClearCache } from '@/data/protocols/cache/IClearCache';
 import { ISaveCache } from '@/data/protocols/cache/ISaveCache';
+import { ILoadCache } from '@/data/protocols/cache/ILoadCache';
 
-export class RedisAdapter implements IClearCache, ISaveCache {
+export class RedisAdapter implements IClearCache, ISaveCache, ILoadCache {
   constructor(private readonly client: RedisClient) {}
 
   async clear(key: string): Promise<void> {
@@ -11,5 +12,11 @@ export class RedisAdapter implements IClearCache, ISaveCache {
 
   async save(key: string, data: any): Promise<void> {
     await this.client.set(key, JSON.stringify(data));
+  }
+
+  async load<T>(key: string): Promise<T> {
+    await this.client.get(key);
+
+    return {} as T;
   }
 }
