08136965c27d4d6e8db26972aac6a1306ded9b4f
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-14 22:06:10 -0300

    test: ensure PostsRepository findByUser returns

--- src/infra/database/typeorm/repositories/PostsRepository.spec.ts
+++ src/infra/database/typeorm/repositories/PostsRepository.spec.ts
@@ -2,6 +2,7 @@ import { mockCreatePostParams } from '@/data/test';
 import { mockCreateUserParams } from '@/domain/test';
 import { Connection, getRepository } from 'typeorm';
 import createConnection from '../connection';
+import { Post } from '../entities/Post';
 import { User } from '../entities/User';
 import { PostsRepository } from './PostsRepository';
 
@@ -45,5 +46,33 @@ describe('PostRepository Test', () => {
     expect(post).toBeTruthy();
     expect(post).toHaveProperty('id');
     expect(post.id).toBeTruthy();
+  });
+
+  it('should list all posts', async () => {
+    const userRepository = getRepository(User);
+
+    const fakeUser = userRepository.create(mockCreateUserParams());
+
+    await userRepository.save(fakeUser);
+
+    const postRepository = getRepository(Post);
+
+    const createPostParams = mockCreatePostParams();
+
+    const fakePost = postRepository.create({
+      ...createPostParams,
+      userId: fakeUser.id,
+    });
+
+    await postRepository.save(fakePost);
+
+    const sut = makeSut();
+
+    const posts = await sut.findByUser(fakeUser.id);
+
+    expect(posts).toBeTruthy();
+    expect(posts).toHaveLength(1);
+    expect(posts[0].title).toBe(createPostParams.title);
+    expect(posts[0].content).toBe(createPostParams.content);
   });
 });
