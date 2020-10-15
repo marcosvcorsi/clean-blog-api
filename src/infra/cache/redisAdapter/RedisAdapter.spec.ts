1887250c6c366bac5d60bec1840b7b6e3296734a
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-14 22:09:35 -0300

    refactor: move redis test to a single describe

--- src/infra/cache/redisAdapter/RedisAdapter.spec.ts
+++ src/infra/cache/redisAdapter/RedisAdapter.spec.ts
@@ -12,23 +12,25 @@ const makeSut = () => {
 };
 
 describe('RedisAdapter Test', () => {
-  it('should call RedisClient del with correct value', async () => {
-    const { sut, clientStub } = makeSut();
+  describe('clear()', () => {
+    it('should call RedisClient del with correct value', async () => {
+      const { sut, clientStub } = makeSut();
 
-    const clearSpy = jest.spyOn(clientStub, 'del');
+      const clearSpy = jest.spyOn(clientStub, 'del');
 
-    await sut.clear('anykey');
+      await sut.clear('anykey');
 
-    expect(clearSpy).toHaveBeenCalledWith('anykey');
-  });
+      expect(clearSpy).toHaveBeenCalledWith('anykey');
+    });
 
-  it('should throw if RedisClient del throws', async () => {
-    const { sut, clientStub } = makeSut();
+    it('should throw if RedisClient del throws', async () => {
+      const { sut, clientStub } = makeSut();
 
-    jest
-      .spyOn(clientStub, 'del')
-      .mockReturnValueOnce(Promise.reject(new Error()));
+      jest
+        .spyOn(clientStub, 'del')
+        .mockReturnValueOnce(Promise.reject(new Error()));
 
-    await expect(sut.clear('anykey')).rejects.toThrow();
+      await expect(sut.clear('anykey')).rejects.toThrow();
+    });
   });
 });
