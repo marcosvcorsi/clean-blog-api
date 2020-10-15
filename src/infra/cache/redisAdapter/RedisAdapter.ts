91e2118ef38b00934efd742ab637898e12049528
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-14 22:25:38 -0300

    feat: ensure RedisAdapter on save call client set

--- src/infra/cache/redisAdapter/RedisAdapter.ts
+++ src/infra/cache/redisAdapter/RedisAdapter.ts
@@ -1,10 +1,15 @@
 import { Redis as RedisClient } from 'ioredis';
 import { IClearCache } from '@/data/protocols/cache/IClearCache';
+import { ISaveCache } from '@/data/protocols/cache/ISaveCache';
 
-export class RedisAdapter implements IClearCache {
+export class RedisAdapter implements IClearCache, ISaveCache {
   constructor(private readonly client: RedisClient) {}
 
   async clear(key: string): Promise<void> {
     await this.client.del(key);
+  }
+
+  async save(key: string, data: any): Promise<void> {
+    await this.client.set(key, JSON.stringify(data));
   }
 }
