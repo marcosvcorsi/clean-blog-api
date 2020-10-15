7fed6227ba00800a64645c7044b1b1fa57078857
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-14 22:05:50 -0300

    feat: ensure PostsRepository findByUser returns

--- src/infra/database/typeorm/repositories/PostsRepository.ts
+++ src/infra/database/typeorm/repositories/PostsRepository.ts
@@ -1,9 +1,11 @@
 import { ICreatePostRepository } from '@/data/protocols/database/posts/ICreatePostRepository';
+import { IFindPostsByUserRepository } from '@/data/protocols/database/posts/IFindPostsByUserRepository';
 import { CreatePostParams } from '@/domain/useCases/posts/ICreatePost';
 import { getRepository, Repository } from 'typeorm';
 import { Post } from '../entities/Post';
 
-export class PostsRepository implements ICreatePostRepository {
+export class PostsRepository
+  implements ICreatePostRepository, IFindPostsByUserRepository {
   private readonly postsRepository: Repository<Post>;
 
   constructor() {
@@ -16,5 +18,15 @@ export class PostsRepository implements ICreatePostRepository {
     await this.postsRepository.save(post);
 
     return post;
+  }
+
+  async findByUser(userId: number): Promise<Post[]> {
+    const posts = await this.postsRepository.find({
+      where: {
+        userId,
+      },
+    });
+
+    return posts;
   }
 }
