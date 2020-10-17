8d990d35989cfbf641b26d9aa83b9f9e2aa531e9
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-17 20:53:47 -0300

    feat: ensure get posts route return forbidden without authorization

--- src/main/app/routes/posts/posts.routes.ts
+++ src/main/app/routes/posts/posts.routes.ts
@@ -6,5 +6,6 @@ import { auth } from '../../middlewares/auth';
 const postsRouter = Router();
 
 postsRouter.post('/', auth, adaptRoute(makeCreatePostController()));
+postsRouter.get('/', auth, (request, response) => response.send());
 
 export default postsRouter;
