82b898cd253d29f19d8f7d1319be537e9a5c02dd
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-17 20:48:31 -0300

    refactor: move post integration tests to a describe

--- src/main/app/routes/posts/posts.routes.test.ts
+++ src/main/app/routes/posts/posts.routes.test.ts
@@ -27,38 +27,40 @@ describe('Posts Routes Test', () => {
     await connection.close();
   });
 
-  it('should return forbidden on POST /posts without Authorization', async () => {
-    const response = await request(app).post('/posts').send({
-      title: 'anytitle',
-      content: 'anycontent',
-    });
-
-    expect(response.status).toBe(403);
-  });
-
-  it('should create a new post on POST /posts', async () => {
-    const userRepository = getRepository(User);
-
-    const fakeUser = userRepository.create({
-      name: 'anyname',
-      email: 'anymail@mail.com',
-      password: 'anypassword',
-    });
-
-    await userRepository.save(fakeUser);
-
-    const token = await sign({ userId: fakeUser.id }, authConfig.secret);
-
-    const response = await request(app)
-      .post('/posts')
-      .send({
+  describe('POST /posts', () => {
+    it('should return forbidden on POST /posts without Authorization', async () => {
+      const response = await request(app).post('/posts').send({
         title: 'anytitle',
         content: 'anycontent',
-      })
-      .set('Authorization', `Bearer ${token}`);
+      });
 
-    expect(response.status).toBe(201);
-    expect(response.body).toBeTruthy();
-    expect(response.body).toHaveProperty('id');
+      expect(response.status).toBe(403);
+    });
+
+    it('should create a new post on POST /posts', async () => {
+      const userRepository = getRepository(User);
+
+      const fakeUser = userRepository.create({
+        name: 'anyname',
+        email: 'anymail@mail.com',
+        password: 'anypassword',
+      });
+
+      await userRepository.save(fakeUser);
+
+      const token = await sign({ userId: fakeUser.id }, authConfig.secret);
+
+      const response = await request(app)
+        .post('/posts')
+        .send({
+          title: 'anytitle',
+          content: 'anycontent',
+        })
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(201);
+      expect(response.body).toBeTruthy();
+      expect(response.body).toHaveProperty('id');
+    });
   });
 });
