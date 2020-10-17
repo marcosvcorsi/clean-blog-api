8c62c059e1258e68edf12267178aa88eb9a6e032
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-17 20:31:11 -0300

    feat: ensure FindPostsByUserController return serverError if use case throws

--- src/presentation/controllers/posts/findPostsByUser/FindPostsByUserController.ts
+++ src/presentation/controllers/posts/findPostsByUser/FindPostsByUserController.ts
@@ -1,4 +1,5 @@
 import { IFindPostsByUser } from '@/domain/useCases/posts/IFindPostsByUser';
+import { serverError } from '@/presentation/helpers/http';
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/Http';
 import { IController } from '@/presentation/protocols/IController';
 
@@ -6,10 +7,14 @@ export class FindPostsByUserController implements IController {
   constructor(private readonly findPostsByUser: IFindPostsByUser) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { userId } = httpRequest;
+    try {
+      const { userId } = httpRequest;
 
-    await this.findPostsByUser.findByUser(Number(userId));
+      await this.findPostsByUser.findByUser(Number(userId));
 
-    return {} as HttpResponse;
+      return {} as HttpResponse;
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
