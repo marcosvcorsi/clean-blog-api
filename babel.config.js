db3cc202e6da3b5dfd14b49523b3c693de2ff914
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-17 23:48:50 -0300

    chore: add babel and config to build

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
